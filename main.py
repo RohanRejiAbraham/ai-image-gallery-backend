2d7e53af9abf3c0ca2d8e84d05d95adf0db8dc23
Rohan <rohanreji619@gmail.com>
2025-12-28 13:06:00 +0530

    origin change

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"], 
+    allow_origins=["https://ai-image-galleryui-2ayidjfi5-rohans-projects-28969eb7.vercel.app"], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
